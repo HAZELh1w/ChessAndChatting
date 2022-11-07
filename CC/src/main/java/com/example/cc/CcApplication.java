8ea070fb255460c1c59247484845421ef2b65cdd
hlw <73787233+HAZELh1w@users.noreply.github.com>
2022-11-07 22:08:18 +0800

    Add files via upload

---
+++ CC/src/main/java/com/example/cc/CcApplication.java
@@ -0,0 +1,15 @@
+package com.example.cc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.cc.mapper")
+public class CcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcApplication.class, args);
+    }
+
+}
